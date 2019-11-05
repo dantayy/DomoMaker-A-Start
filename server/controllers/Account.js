921ef9a20b2e57140d3aa2d2e5101ce1f9fabd34
dantayy <nvm6501@rit.edu>
2019-11-05 15:48:08 -0500

    Completed DomoMaker part D

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -6,10 +6,6 @@ const loginPage = (req, res) => {
   res.render('login', { csrfToken: req.csrfToken() });
 };
 
-const signupPage = (req, res) => {
-  res.render('signup', { csrfToken: req.csrfToken() });
-};
-
 const logout = (req, res) => {
   req.session.destroy();
   res.redirect('/');
@@ -88,8 +84,16 @@ const signup = (request, response) => {
   });
 };
 
+const getToken = (req, res) => {
+  const csrfJSON = {
+    csrfToken: req.csrfToken(),
+  };
+
+  res.json(csrfJSON);
+};
+
 module.exports.loginPage = loginPage;
 module.exports.login = login;
 module.exports.logout = logout;
-module.exports.signupPage = signupPage;
 module.exports.signup = signup;
+module.exports.getToken = getToken;
